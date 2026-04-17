512c6a57bd6499761ddf056498bd59775cfbef28
Vickey Kumar <vickey15052005@gmail.com>
2026-04-18 03:54:55 +0530

    COMMIT_EDITMSG

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -14,7 +14,7 @@ SELECT
 	job_schedule_type,
 	salary_year_avg,
 	job_posted_date,
-    name AS company_name
+  name AS company_name
 FROM
     job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
@@ -128,4 +128,8 @@ RESULTS
   }
 ]
 */
+
+
+
+
 
